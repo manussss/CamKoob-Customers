4d703c391adf7acaf29b933c988a503f186f8932
ebranquinho <branquinhomanu@gmail.com>
2025-05-03 09:51:29 -0300

    add api endpoints to add and get customers

--- CamKoob.Customers.API/Program.cs
+++ CamKoob.Customers.API/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDatabase(builder.Configuration);
+builder.Services.AddRepositories();
 
 var app = builder.Build();
 
@@ -14,21 +15,62 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/api/v1/customers", async () =>
+app.MapPost("/api/v1/customers", async (
+    [FromServices] ICustomerRepository customerRepository,
+    CreateCustomerDto dto
+) =>
 {
-    
+    var customer = new Customer(dto.CustomerName);
+    var contract = new Contract(dto.Product, customer.Id);
+    customer.SetContract(contract);
+
+    await customerRepository.AddAsync(customer);
+
+    return Results.NoContent();
 })
 .WithOpenApi();
 
-app.MapGet("/api/v1/customers", async () =>
+app.MapGet("/api/v1/customers", async (
+    [FromServices] ICustomerRepository customerRepository
+) =>
 {
+    var customers = await customerRepository.GetAsync();
+
+    if (customers is null || !customers.Any())
+        return Results.NotFound();
     
+    return Results.Ok(customers.Select(x => new GetCustomerDto
+    {
+        Id = x.Id,
+        Name = x.Name,
+        Contract = new GetContractDto
+        {
+            Id = x.Contract.Id,
+            Product = x.Contract.Product
+        }
+    }));
 })
 .WithOpenApi();
 
-app.MapGet("/api/v1/customers/{id}", async (Guid id) =>
+app.MapGet("/api/v1/customers/{id}", async (
+    [FromServices] ICustomerRepository customerRepository,
+    Guid id) =>
 {
+    var customer = await customerRepository.GetByIdAsync(id);
+
+    if (customer is null)
+        return Results.NotFound();
     
+    return Results.Ok(new GetCustomerDto
+    {
+        Id = customer.Id,
+        Name = customer.Name,
+        Contract = new GetContractDto
+        {
+            Id = customer.Contract.Id,
+            Product = customer.Contract.Product
+        }
+    });
 })
 .WithOpenApi();
 
--- CamKoob.Customers.API/Usings.cs
+++ CamKoob.Customers.API/Usings.cs
@@ -1,1 +1,5 @@
 global using CamKoob.Customers.IoC;
+global using CamKoob.Customers.Domain.Interfaces;
+global using Microsoft.AspNetCore.Mvc;
+global using CamKoob.Customers.Application.DTOs;
+global using CamKoob.Customers.Domain.Entities;
